f629c65295b54840b62261b8029aa442023b93e3
Daniel Schwen <daniel@schwen.de>
2015-04-16 19:23:38 -0600

    change env var for spparks

--- contrib/contrib.mk
+++ contrib/contrib.mk
@@ -1,5 +1,5 @@
 # Check the existence of the contrib submodules and build accordingly
-SPPARKS_SUBMODULE    := $(CURDIR)/spparks
-ifneq ($(wildcard $(SPPARKS_SUBMODULE)/src/Makefile),)
+SPPARKS_DIR    ?= $(CURDIR)/spparks
+ifneq ($(wildcard $(SPPARKS_DIR)/src/Makefile),)
   include spparks.mk
 endif
