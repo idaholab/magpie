1e5c4adaab0c43bb1cc34ed5c4592373c4940cab
Daniel Schwen <daniel.schwen@inl.gov>
2015-04-21 09:18:26 -0600

    Fix build system

--- magpie.mk
+++ magpie.mk
@@ -1,10 +1,10 @@
 # add contrib to include path
-app_INCLUDES   += -I $(APPLICATION_DIR)contrib
+app_INCLUDES   += -I $(APPLICATION_DIR)/contrib
 
 # Check the existence of the contrib submodules and build accordingly
-SPPARKS_DIR    ?= $(APPLICATION_DIR)contrib/spparks
+SPPARKS_DIR    ?= $(APPLICATION_DIR)/contrib/spparks
 ifneq ($(wildcard $(SPPARKS_DIR)/src/Makefile),)
-  EXTRA_FLAGS += -DSPPARKS_ENABLED
-  include $(APPLICATION_DIR)contrib/spparks.mk
+  ADDITIONAL_CPPFLAGS += -DSPPARKS_ENABLED
+  include $(APPLICATION_DIR)/contrib/spparks.mk
 endif
 
