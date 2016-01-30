66f0b2643e73d81350e23b1548448f527444eeca
Daniel Schwen <daniel.schwen@inl.gov>
2016-01-29 17:12:39 -0700

    Add helper functions to PKA generator (#24)

--- include/userobjects/PKAGeneratorBase.h
+++ include/userobjects/PKAGeneratorBase.h
@@ -23,6 +23,16 @@ public:
    * The element volume is passed in as it is computed in the MyTRIMRasterizer anyways.
    */
   virtual void appendPKAs(std::vector<MyTRIM_NS::ionBase> & ion_list, Real dt, Real vol) const = 0;
+
+protected:
+  /// helper function to set the ion position to a random location in the current element
+  void setPosition(MyTRIM_NS::ionBase & ion);
+
+  /// helper function to set the ion direction to a random direction
+  void setRandomDirection(MyTRIM_NS::ionBase & ion);
+
+  /// Return a point with random uniformly distributed coordinates in the unit cube
+  Point getRandomPoint() { return Point(getRandomReal(), getRandomReal(), getRandomReal()); }
 };
 
 #endif // PKAGENERATORBASE_H
