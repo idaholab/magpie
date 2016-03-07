81a85215c07a17c2f86b26a2c23572aceeba88ec
Sebastian Schunert <snschune@ncsu.edu>
2016-03-07 15:17:26 -0700

    Isotropic sampling of direction without rejection (#61)

--- src/userobjects/PKAGeneratorBase.C
+++ src/userobjects/PKAGeneratorBase.C
@@ -23,18 +23,8 @@ PKAGeneratorBase::setPosition(MyTRIM_NS::IonBase & ion) const
 void
 PKAGeneratorBase::setRandomDirection(MyTRIM_NS::IonBase & ion) const
 {
-  Real norm_sq;
-
-  do
-  {
-    ion._dir = getRandomPoint() - Point(0.5, 0.5, 0.5);
-    norm_sq = ion._dir.norm_sq();
-
-    // we reject points outside or the sphere with radius 1/2 (otherwise
-    // there'd be a higher probability to point towards the cube corners) and
-    // points with small norms (for numerical reasons).
-  } while (norm_sq < 0.001 || norm_sq > 0.25);
-
-  // normalize direction vector
-  ion._dir /= std::sqrt(norm_sq);
+  const Real cosTheta = 2.0 * getRandomReal() - 1.0;
+  const Real sinTheta = std::sqrt(1.0 - cosTheta * cosTheta);
+  const Real phi = 2.0 * libMesh::pi * getRandomReal();
+  ion._dir = Point(sinTheta * std::cos(phi), sinTheta * std::sin(phi), cosTheta);
 }
