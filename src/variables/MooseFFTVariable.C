4d02241503922dafa79c0b4346a83e8d170e59e1
Daniel Schwen <daniel.schwen@inl.gov>
2020-01-20 13:01:02 -0700

    Add variable shim for FFT (#401)

---
+++ src/variables/MooseFFTVariable.C
@@ -0,0 +1,20 @@
+/**********************************************************************/
+/*                     DO NOT MODIFY THIS HEADER                      */
+/* MAGPIE - Mesoscale Atomistic Glue Program for Integrated Execution */
+/*                                                                    */
+/*            Copyright 2017 Battelle Energy Alliance, LLC            */
+/*                        ALL RIGHTS RESERVED                         */
+/**********************************************************************/
+
+#include <MooseFFTVariable.h>
+
+InputParameters
+MooseFFTVariable::validParams()
+{
+  InputParameters params = MooseVariableFEBase::validParams();
+  return params;
+}
+
+MooseFFTVariable::MooseFFTVariable(const InputParameters & parameters) : MooseVariable(parameters)
+{
+}
