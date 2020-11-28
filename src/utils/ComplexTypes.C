be466cd9271746b4ae3ccee4713636ed1e8cc01f
Antonio Recuero <amrecuero85@gmail.com>
2020-11-28 15:10:14 -0700

    Disable complex specialization of fillOrthotropicTensor. Of no use right now.

--- src/utils/ComplexTypes.C
+++ src/utils/ComplexTypes.C
@@ -47,65 +47,10 @@ template class VectorValue<Complex>;
 template <>
 void
 RankFourTensorTempl<Complex>::fillGeneralOrthotropicFromInputVector(
-    const std::vector<Complex> & input)
+    const std::vector<Complex> & /*input*/)
 {
-  if (input.size() != 12)
-    mooseError("To use fillGeneralOrhotropicFromInputVector, your input must have size 12. Yours "
-               "has size ",
-               input.size());
-
-  const Complex & Ea = input[0];
-  const Complex & Eb = input[1];
-  const Complex & Ec = input[2];
-  const Complex & Gab = input[3];
-  const Complex & Gbc = input[4];
-  const Complex & Gca = input[5];
-  const Complex & nuba = input[6];
-  const Complex & nuca = input[7];
-  const Complex & nucb = input[8];
-  const Complex & nuab = input[9];
-  const Complex & nuac = input[10];
-  const Complex & nubc = input[11];
-
-  unsigned int ntens = N * (N + 1) / 2;
-
-  std::vector<Complex> mat;
-  mat.assign(ntens * ntens, 0);
-
-  Complex k = 1 - nuab * nuba - nubc * nucb - nuca * nuac - nuab * nubc * nuca - nuba * nucb * nuac;
-
-  mat[0] = Ea * (1 - nubc * nucb) / k;
-  mat[1] = Ea * (nubc * nuca + nuba) / k;
-  mat[2] = Ea * (nuba * nucb + nuca) / k;
-
-  mat[6] = Eb * (nuac * nucb + nuab) / k;
-  mat[7] = Eb * (1 - nuac * nuca) / k;
-  mat[8] = Eb * (nuab * nuca + nucb) / k;
-
-  mat[12] = Ec * (nuab * nubc + nuac) / k;
-  mat[13] = Ec * (nuac * nuba + nubc) / k;
-  mat[14] = Ec * (1 - nuab * nuba) / k;
-
-  mat[21] = 2 * Gab;
-  mat[28] = 2 * Gca;
-  mat[35] = 2 * Gbc;
-
-  int nskip = N - 1;
-
-  unsigned int index = 0;
-  for (unsigned int i = 0; i < N; ++i)
-    for (unsigned int j = 0; j < N; ++j)
-      for (unsigned int k = 0; k < N; ++k)
-        for (unsigned int l = 0; l < N; ++l)
-        {
-          if (i == j)
-            (*this)._vals[index] =
-                k == l ? mat[i * ntens + k] : mat[i * ntens + k + nskip + l] / 2.0;
-          else
-            (*this)._vals[index] = k == l ? mat[(nskip + i + j) * ntens + k]
-                                          : mat[(nskip + i + j) * ntens + k + nskip + l] / 2.0;
-          index++;
-        }
+  mooseError("RankFourTensorTempl<>::fillGeneralOrthotropicFromInputVector is only to be "
+             "used to fill elasticity tensors with real numbers");
 }
 
 template class RankTwoTensorTempl<Complex>;
